77ed4b08b1a260ef8d7be27a66d478dc4a6b2765
Dustin Pierson <dustinapierson@gmail.com>
2018-01-28 23:40:53 -0600

    finished individual film page

--- src/components/Film.jsx
+++ src/components/Film.jsx
@@ -10,27 +10,62 @@ class Film extends Component{
         this.state={
 
         }
+    
     }
-
-
-
-
-
-
-
-
-
-
-
+        componentDidMount() {
+            fetch(`https://ghibliapi.herokuapp.com/films/${this.props.match.params.id}`)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    
+                    
+                        
+                    this.setState(data);
+                    
+                    console.log(this.state);
+                   
+                })
+    
+    
+    
+    
+    
+    
+    
+    }
 
 
 
 
     render(){
         return(
-            <div>
-            <h1> Single Film</h1>
-        </div>
+            <React.Fragment>
+                <div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">{this.state.title}</h1>
+    <p className="lead"> Director : {this.state.director}</p>
+  </div>
+</div>
+
+<div className="card text-center">
+  <div className="card-header">
+    Released: {this.state.release_date}
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Synopsis</h5>
+    <p className="card-text"> {this.state.description}</p>
+  </div>
+  <div>
+      <p> Producer : {this.state.producer}</p>
+      </div>
+  <div className="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+
+</React.Fragment>
         )
     }
 }
